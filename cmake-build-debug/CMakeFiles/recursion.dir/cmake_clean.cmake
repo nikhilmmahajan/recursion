11a982c9ad9ce71a9862073c29ab4ca700ad5fa1
nikhilmmahajan <nikhil.mm@gmail.com>
2018-02-19 10:56:08 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/recursion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/recursion.dir/main.cpp.obj"
+  "recursion.pdb"
+  "recursion.exe"
+  "recursion.exe.manifest"
+  "librecursion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/recursion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
